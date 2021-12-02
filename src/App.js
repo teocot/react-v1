049804716f776a84f3ad48fb0f315062f7584ace
teocot <teodor@xtra.co.nz>
2021-12-03 00:39:55 +1300

    $ npm run deploy
        > react-v1@0.1.0 predeploy C:\dev\react-v1
        > npm run build
    
        > react-v1@0.1.0 build C:\dev\react-v1
        > react-scripts build
    
        Creating an optimized production build...
        Compiled successfully.
    
        File sizes after gzip:
    
          41.34 KB  build\static\js\2.84b431ea.chunk.js
            1.63 KB   build\static\js\3.d177488b.chunk.js
    	  1.17 KB   build\static\js\runtime-main.4e3ecf3a.js
    	    513 B     build\static\js\main.73f72aa2.chunk.js
    	      278 B     build\static\css\main.6dea0f05.chunk.css
    
    	      The project was built assuming it is hosted at /react-v1/.
    	      You can control this with the homepage field in your
    	      package.json.
    
    	      The build folder is ready to be deployed.
    
    	      Find out more about deployment here:
    
    	        https://cra.link/deployment
    
    		> react-v1@0.1.0 deploy C:\dev\react-v1
    		> gh-pages -d build
    
    		Published

--- src/App.js
+++ src/App.js
@@ -19,6 +19,20 @@ function App() {
         <ComponentTwo role="Employee" />
       </div>
       <ComponentTwo role="Subordonate" />
+      <p>
+        $ npm run deploy react-v1@0.1.0 predeploy C:\dev\sean\react-v1 npm run
+        build react-v1@0.1.0 build C:\dev\sean\react-v1 react-scripts build
+        Creating an optimized production build... Compiled successfully. File
+        sizes after gzip: 41.34 KB build\static\js\2.84b431ea.chunk.js 1.63 KB
+        build\static\js\3.d177488b.chunk.js 1.17 KB
+        build\static\js\runtime-main.4e3ecf3a.js 513 B
+        build\static\js\main.73f72aa2.chunk.js 278 B
+        build\static\css\main.6dea0f05.chunk.css The project was built assuming
+        it is hosted at /react-v1/. You can control this with the homepage field
+        in your package.json. The build folder is ready to be deployed. Find out
+        more about deployment here: https://cra.link/deployment react-v1@0.1.0
+        deploy C:\dev\sean\react-v1 gh-pages -d build Published
+      </p>
     </div>
   );
 }
