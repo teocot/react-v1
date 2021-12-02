1ff3533fe3945f40a89ef7076d452850969494b2
teocot <teodor@xtra.co.nz>
2021-12-03 00:45:52 +1300

    pipeline test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom";
 import React from "react";
 
 function Welcome(props) {
-  return <h1>Helloyee, {props.name}</h1>;
+  return <h1>Helloyeem, {props.name}</h1>;
 }
 function ComponentTwo(props) {
   return <h1>This is {props.role}</h1>;
